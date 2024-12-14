6470431fe91a00a6baf380e2d71c995edded1160
flaviot097 <112975530+flaviot097@users.noreply.github.com>
2024-12-14 00:28:44 -0300

    Agregar archivos de proyecto.

---
+++ TableProductPanel.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace dataGridView_App
+{
+    partial class TableProductPanel
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idproductDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameproductDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codebarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.routeImgDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionproductDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.costDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.marginrevenueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idcategoryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.app_gestor_cajaDataSet = new dataGridView_App.App_gestor_cajaDataSet();
+            this.productTableAdapter = new dataGridView_App.App_gestor_cajaDataSetTableAdapters.ProductTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.app_gestor_cajaDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idproductDataGridViewTextBoxColumn,
+            this.nameproductDataGridViewTextBoxColumn,
+            this.codebarDataGridViewTextBoxColumn,
+            this.routeImgDataGridViewTextBoxColumn,
+            this.descriptionproductDataGridViewTextBoxColumn,
+            this.stockDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.costDataGridViewTextBoxColumn,
+            this.marginrevenueDataGridViewTextBoxColumn,
+            this.idcategoryDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.productBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(54, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(691, 394);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idproductDataGridViewTextBoxColumn
+            // 
+            this.idproductDataGridViewTextBoxColumn.DataPropertyName = "Id_product";
+            this.idproductDataGridViewTextBoxColumn.HeaderText = "Id_product";
+            this.idproductDataGridViewTextBoxColumn.Name = "idproductDataGridViewTextBoxColumn";
+            this.idproductDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameproductDataGridViewTextBoxColumn
+            // 
+            this.nameproductDataGridViewTextBoxColumn.DataPropertyName = "name_product";
+            this.nameproductDataGridViewTextBoxColumn.HeaderText = "name_product";
+            this.nameproductDataGridViewTextBoxColumn.Name = "nameproductDataGridViewTextBoxColumn";
+            this.nameproductDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // codebarDataGridViewTextBoxColumn
+            // 
+            this.codebarDataGridViewTextBoxColumn.DataPropertyName = "Code_bar";
+            this.codebarDataGridViewTextBoxColumn.HeaderText = "Code_bar";
+            this.codebarDataGridViewTextBoxColumn.Name = "codebarDataGridViewTextBoxColumn";
+            this.codebarDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // routeImgDataGridViewTextBoxColumn
+            // 
+            this.routeImgDataGridViewTextBoxColumn.DataPropertyName = "RouteImg";
+            this.routeImgDataGridViewTextBoxColumn.HeaderText = "RouteImg";
+            this.routeImgDataGridViewTextBoxColumn.Name = "routeImgDataGridViewTextBoxColumn";
+            this.routeImgDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionproductDataGridViewTextBoxColumn
+            // 
+            this.descriptionproductDataGridViewTextBoxColumn.DataPropertyName = "description_product";
+            this.descriptionproductDataGridViewTextBoxColumn.HeaderText = "description_product";
+            this.descriptionproductDataGridViewTextBoxColumn.Name = "descriptionproductDataGridViewTextBoxColumn";
+            this.descriptionproductDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // stockDataGridViewTextBoxColumn
+            // 
+            this.stockDataGridViewTextBoxColumn.DataPropertyName = "stock";
+            this.stockDataGridViewTextBoxColumn.HeaderText = "stock";
+            this.stockDataGridViewTextBoxColumn.Name = "stockDataGridViewTextBoxColumn";
+            this.stockDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            this.priceDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // costDataGridViewTextBoxColumn
+            // 
+            this.costDataGridViewTextBoxColumn.DataPropertyName = "cost";
+            this.costDataGridViewTextBoxColumn.HeaderText = "cost";
+            this.costDataGridViewTextBoxColumn.Name = "costDataGridViewTextBoxColumn";
+            this.costDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // marginrevenueDataGridViewTextBoxColumn
+            // 
+            this.marginrevenueDataGridViewTextBoxColumn.DataPropertyName = "margin_revenue";
+            this.marginrevenueDataGridViewTextBoxColumn.HeaderText = "margin_revenue";
+            this.marginrevenueDataGridViewTextBoxColumn.Name = "marginrevenueDataGridViewTextBoxColumn";
+            this.marginrevenueDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // idcategoryDataGridViewTextBoxColumn
+            // 
+            this.idcategoryDataGridViewTextBoxColumn.DataPropertyName = "id_category";
+            this.idcategoryDataGridViewTextBoxColumn.HeaderText = "id_category";
+            this.idcategoryDataGridViewTextBoxColumn.Name = "idcategoryDataGridViewTextBoxColumn";
+            this.idcategoryDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataMember = "Product";
+            this.productBindingSource.DataSource = this.app_gestor_cajaDataSet;
+            // 
+            // app_gestor_cajaDataSet
+            // 
+            this.app_gestor_cajaDataSet.DataSetName = "App_gestor_cajaDataSet";
+            this.app_gestor_cajaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productTableAdapter
+            // 
+            this.productTableAdapter.ClearBeforeFill = true;
+            // 
+            // TableProductPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "TableProductPanel";
+            this.Text = "Tabla de datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.app_gestor_cajaDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private App_gestor_cajaDataSet app_gestor_cajaDataSet;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private App_gestor_cajaDataSetTableAdapters.ProductTableAdapter productTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idproductDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameproductDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codebarDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn routeImgDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionproductDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stockDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn costDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn marginrevenueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idcategoryDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ TableProductPanel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataGridView_App
+{
+    public partial class TableProductPanel : Form
+    {
+        public TableProductPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'app_gestor_cajaDataSet.Product' Puede moverla o quitarla según sea necesario.
+            this.productTableAdapter.Fill(this.app_gestor_cajaDataSet.Product);
+
+        }
+
+    }
+}
